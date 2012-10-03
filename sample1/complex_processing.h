5d8f23f62a46e28e0add27c630cb92cc0b840eb3
Pierre FILSTROFF <pfilstroff@gmail.com>
2012-10-03 18:25:39 +0200

    Updated files

---
+++ sample1/complex_processing.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <complex>
+using namespace std;
+
+class ComplexProcessing {
+	public:
+		ComplexProcessing();
+		complex<double> addition(complex<double>, complex<double>);
+		complex<double> subtraction(complex<double>, complex<double>);
+};
--- sample1/main.cpp
+++ sample1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <complex>
+#include "complex_processing.h"
 using namespace std;
 
 
@@ -11,18 +12,25 @@ int addition(int a, int b) {
 
 int main ()
 {
-	complex<int> z(1, 2);
-	complex<int> zz,I(0, 1); 
-	zz=3+I*4; 
-	complex<int> zzz = z + zz;
-	cout<<z<<"+"<<zz<<"="<<zzz<<endl;  
-	cout<<"Complexe Conjugue "<<conj(zzz)<<endl; 
-	cout<<"Partie Reelle "<<real(zzz)<<endl; 
-	cout<<"Partie Imaginaire "<<imag(zzz)<<endl; 
-	cout<<"Module Carre "<<norm(zzz)<<endl; 
-	cout<<"Module "<<abs(zzz)<<endl; 
-	cout<<"Argument "<<arg(zzz)<<endl; 
-	cout<<"I*I="<<I*I<<endl; 
-	cout<<"sqrt(-1)="<<sqrt(complex<int>(-1))<<endl; 
-	return 0;
+
+	ComplexProcessing cp;
+	complex<double> a(1, 2);
+	complex<double> b(1, 2);
+	complex<double> result = cp.addition(a, b);
+
+	cout << result << endl;
+	// complex<int> z(1, 2);
+	// complex<int> zz,I(0, 1); 
+	// zz=3+I*4; 
+	// complex<int> zzz = z + zz;
+	// cout<<z<<"+"<<zz<<"="<<zzz<<endl;  
+	// cout<<"Complexe Conjugue "<<conj(zzz)<<endl; 
+	// cout<<"Partie Reelle "<<real(zzz)<<endl; 
+	// cout<<"Partie Imaginaire "<<imag(zzz)<<endl; 
+	// cout<<"Module Carre "<<norm(zzz)<<endl; 
+	// cout<<"Module "<<abs(zzz)<<endl; 
+	// cout<<"Argument "<<arg(zzz)<<endl; 
+	// cout<<"I*I="<<I*I<<endl; 
+	// cout<<"sqrt(-1)="<<sqrt(complex<int>(-1))<<endl; 
+	// return 0;
 }
