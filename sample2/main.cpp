e462ed8865f07ce856d81e187aa502afba6de316
Pierre FILSTROFF <pfilstroff@gmail.com>
2012-11-14 09:45:11 +0100

    Simplify sample2

--- sample2/main.cpp
+++ sample2/main.cpp
@@ -1,31 +1,76 @@
 #include <iostream>
-#include "tools.hpp"
+
+/**
+ * @brief Somme
+ * @param a
+ * @param b
+ * @return 
+ */
+double somme(double a, double b)
+{
+   return a + b; 
+} 
+
+/**
+ * @brief Somme2
+ * @param a
+ * @param b
+ * @param m_result
+ */
+void somme_2(double a, double b, double& m_result)
+{
+   m_result = a + b;
+} 
+
+/**
+ * @brief Max
+ * @param a
+ * @param b
+ * @return 
+ */
+double max(double a, double b)
+{
+    double max;
+    max = (a > b) ? a : b;
+
+    return max;
+}
+
+/**
+ * @brief Max2
+ * @param a
+ * @param b
+ * @return 
+ */
+double& max_2(double a, double b)
+{
+    return (a > b) ? a : b;
+}
 
 int main () {
 
     std::cout << "==== Sample 2 ====" << std::endl;
     double a = 12;
     double b = 23;
-    Tools t;
 
-    double result_somme1 = t.somme(a, b);
+    double result_somme1 = somme(a, b);
     std::cout << "somme(" << a << ", " << b << ") = " << result_somme1 << std::endl;
 
     a = 6;
     b = 53;
     double result_somme2;
-    t.somme2(a, b, &result_somme2);
+    somme_2(a, b, result_somme2);
     std::cout << "somme(" << a << ", " << b << ", &result). result = " << result_somme2 << std::endl;
 
     a = 96;
     b = 138;
-    double result_max = t.max(a, b);
+    double result_max = max(a, b);
     std::cout << "max(" << a << ", " << b << ") = " << result_max << std::endl;
 
     a = 376;
     b = 3;
-    const double& result_max2 = t.max2(&a, &b);
+    double& result_max2 = max_2(a, b);
     std::cout << "max(" << a << ", " << b << ") = " << result_max2 << std::endl;
 
-	return 0;
+    return 0;
 }
