c647a24ced1f954fe70499c1bbbcf4e8ccdcf032
Pierre FILSTROFF <pfilstroff@gmail.com>
2013-02-04 17:35:26 +0100

    Initial commit for sample 4

---
+++ sample4/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+namespace
+{
+  template <typename Type>
+    class MyClassBase
+    {
+      public:
+        typedef Type value_type;
+
+        MyClassBase(const Type& value) : m_value(value) {}
+        const Type& value() const { return m_value; }
+
+      private:
+        Type m_value;
+    };
+}
+
+namespace alpha
+{
+  class MyClass : public MyClassBase<std::string>
+  {
+    public:
+      MyClass(const MyClass::value_type& value) : MyClassBase(value) {};
+  };
+}
+
+namespace beta
+{
+  class MyClass : public MyClassBase<double>
+  {
+    public:
+      MyClass(const MyClass::value_type& value) : MyClassBase(value) {};
+  };
+}
+
+namespace gamma
+{
+  template <typename T>
+    void print(const T&)
+    {
+      std::cout << "IOS is for dummies." << std::endl;
+    }
+
+  void run()
+  {
+    alpha::MyClass mc1("coucou");
+    beta::MyClass mc2(3.14f);
+
+    print(mc1);
+    print(mc2);
+  }
+}
+
+int main()
+{
+  gamma::run();
+
+  return EXIT_SUCCESS;
+}
